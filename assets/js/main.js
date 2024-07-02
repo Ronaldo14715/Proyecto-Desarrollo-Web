6ffca80c1604ac4bd9ed0596822c7f854eefe3f8
Ronaldo14715 <ronaldo.farfan.1998@gmail.com>
2024-07-01 23:39:00 -0500

    agregando nuevas cosas a la pagina de ventas y agregandos mixins para los display flex

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,6 @@
-const abrir = document.querySelector('#abrir');
-const cerrar = document.querySelector('#cerrar');
-const nav = document.querySelector('#main-nav');
+const abrir = document.getElementById('abrir');
+const cerrar = document.getElementById('cerrar');
+const nav = document.getElementById('#ain-nav');
 
 abrir.addEventListener('click', () => {
     nav.classList.add("visible");
@@ -8,4 +8,20 @@ abrir.addEventListener('click', () => {
 
 cerrar.addEventListener('click', () => {
     nav.classList.remove("visible");
+});
+
+
+
+let btnFigura = document.getElementById('producto-btn-figura');
+let btnFunkoPop = document.getElementById('producto-btn-funkoPop');
+let btnJuegos = document.getElementById('producto-btn-juegos');
+let btnAccesorios = document.getElementById('producto-btn-accesorios');
+
+let divFigura = document.getElementById('contenedor-producto-figura');
+let divFunkoPop = document.getElementById('contenedor-producto-funkoPop');
+let divjuegos = document.getElementById('contenedor-producto-juegos');
+let divAccesorios = document.getElementById('contenedor-producto-accesorios');
+
+btnFigura.addEventListener('click', () => {
+    
 });
