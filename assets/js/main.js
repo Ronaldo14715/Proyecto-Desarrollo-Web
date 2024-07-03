382040f61bfea3b8a4a8eaeedaba42c1a88227f2
RonaldoFarfan14715 <ronaldofarfan14715@gmail.com>
2024-07-03 17:26:10 -0500

    ventas 3.0 falta agregar info

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,21 @@
-const abrir = document.getElementById('abrir');
-const cerrar = document.getElementById('cerrar');
-const nav = document.getElementById('#ain-nav');
+// Variables para el NAV
+let abrir = document.getElementById('abrir');
+let cerrar = document.getElementById('cerrar');
+let nav = document.getElementById('ain-nav');
 
+// Variables para los botones - VENTAS
+let btnFigura = document.getElementById('producto-btn-figura');
+let btnFunkoPop = document.getElementById('producto-btn-funkoPop');
+let btnJuegos = document.getElementById('producto-btn-juegos');
+let btnAccesorios = document.getElementById('producto-btn-accesorios');
+
+// Variables para los DIVS - VENTAS
+let divFigura = document.getElementById('contenedor-producto-figura');
+let divFunkoPop = document.getElementById('contenedor-producto-funkoPop');
+let divJuegos = document.getElementById('contenedor-producto-juegos');
+let divAccesorios = document.getElementById('contenedor-producto-accesorios');
+
+// NAV
 abrir.addEventListener('click', () => {
     nav.classList.add("visible");
 });
@@ -10,18 +24,32 @@ cerrar.addEventListener('click', () => {
     nav.classList.remove("visible");
 });
 
+// Contenedor Ventas
+function mostrarContenedorVenta(id) {
+    let contenedorVentas = document.querySelectorAll('.producto');
+    contenedorVentas.forEach(function(value) {
+        value.style.display = 'none';
+    });
 
+    let contenedorSeleccionado = document.getElementById(id);
 
-let btnFigura = document.getElementById('producto-btn-figura');
-let btnFunkoPop = document.getElementById('producto-btn-funkoPop');
-let btnJuegos = document.getElementById('producto-btn-juegos');
-let btnAccesorios = document.getElementById('producto-btn-accesorios');
+    if (contenedorSeleccionado) {
+        contenedorSeleccionado.style.display = 'grid';
+    }
+}
 
-let divFigura = document.getElementById('contenedor-producto-figura');
-let divFunkoPop = document.getElementById('contenedor-producto-funkoPop');
-let divjuegos = document.getElementById('contenedor-producto-juegos');
-let divAccesorios = document.getElementById('contenedor-producto-accesorios');
+btnFigura.addEventListener('click', function() {
+    mostrarContenedorVenta('contenedor-producto-figura');
+});
 
-btnFigura.addEventListener('click', () => {
-    
+btnFunkoPop.addEventListener('click', function() {
+    mostrarContenedorVenta('contenedor-producto-funkoPop');
+});
+
+btnJuegos.addEventListener('click', function() {
+    mostrarContenedorVenta('contenedor-producto-juegos');
+});
+
+btnAccesorios.addEventListener('click', function() {
+    mostrarContenedorVenta('contenedor-producto-accesorios');
 });
