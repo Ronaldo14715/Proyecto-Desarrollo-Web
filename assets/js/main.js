5a6c0c270d5f75274e9995bb38691e8a8477f51c
Ronaldo14715 <ronaldo.farfan.1998@gmail.com>
2024-07-04 00:28:39 -0500

    agregando imagenes y solucionado bug en el js de la pagina ventas

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,56 +1,59 @@
-// Variables para el NAV
 const abrir = document.querySelector('#abrir');
-const cerrar = document.querySelector('#cerrar');
-const nav = document.querySelector('#main-nav');
+    const cerrar = document.querySelector('#cerrar');
+    const nav = document.querySelector('#main-nav');
 
-// Variables para los botones - VENTAS
-let btnFigura = document.getElementById('producto-btn-figura');
-let btnFunkoPop = document.getElementById('producto-btn-funkoPop');
-let btnJuegos = document.getElementById('producto-btn-juegos');
-let btnAccesorios = document.getElementById('producto-btn-accesorios');
+    // Variables para los botones - VENTAS
+    let btnFigura = document.getElementById('producto-btn-figura');
+    let btnFunkoPop = document.getElementById('producto-btn-funkoPop');
+    let btnJuegos = document.getElementById('producto-btn-juegos');
+    let btnAccesorios = document.getElementById('producto-btn-accesorios');
 
-// Variables para los DIVS - VENTAS
-let divFigura = document.getElementById('contenedor-producto-figura');
-let divFunkoPop = document.getElementById('contenedor-producto-funkoPop');
-let divJuegos = document.getElementById('contenedor-producto-juegos');
-let divAccesorios = document.getElementById('contenedor-producto-accesorios');
+    // Variables para los DIVS - VENTAS
+    let divFigura = document.getElementById('contenedor-producto-figura');
+    let divFunkoPop = document.getElementById('contenedor-producto-funkoPop');
+    let divJuegos = document.getElementById('contenedor-producto-juegos');
+    let divAccesorios = document.getElementById('contenedor-producto-accesorios');
 
-// NAV
-abrir.addEventListener('click', () => {
-    nav.classList.add("visible");
-});
-
-cerrar.addEventListener('click', () => {
-    nav.classList.remove("visible");
-});
-
-
-// Contenedor Ventas
-function mostrarContenedorVenta(id) {
-    let contenedorVentas = document.querySelectorAll('.producto');
-    contenedorVentas.forEach(function(value) {
-        value.style.display = 'none';
+    // NAV
+    abrir.addEventListener('click', () => {
+        nav.classList.add("visible");
     });
 
-    let contenedorSeleccionado = document.getElementById(id);
+    cerrar.addEventListener('click', () => {
+        nav.classList.remove("visible");
+    });
 
-    if (contenedorSeleccionado) {
-        contenedorSeleccionado.style.display = 'grid';
+    function mostrarContenedorVenta(id, btn) {
+        let contenedorVentas = document.querySelectorAll('.producto');
+        contenedorVentas.forEach(function(value) {
+            value.style.display = 'none';
+        });
+
+        let contenedorSeleccionado = document.getElementById(id);
+        if (contenedorSeleccionado) {
+            contenedorSeleccionado.style.display = 'grid';
+        }
+
+        let botones = document.querySelectorAll('button[id^="producto-btn-"]');
+        botones.forEach(function(value) {
+            value.classList.remove('visible');
+        });
+
+        btn.classList.add('visible');
     }
-}
 
-btnFigura.addEventListener('click', function() {
-    mostrarContenedorVenta('contenedor-producto-figura');
-});
+    btnFigura.addEventListener('click', function() {
+        mostrarContenedorVenta('contenedor-producto-figura', this);
+    });
 
-btnFunkoPop.addEventListener('click', function() {
-    mostrarContenedorVenta('contenedor-producto-funkoPop');
-});
+    btnFunkoPop.addEventListener('click', function() {
+        mostrarContenedorVenta('contenedor-producto-funkoPop', this);
+    });
 
-btnJuegos.addEventListener('click', function() {
-    mostrarContenedorVenta('contenedor-producto-juegos');
-});
+    btnJuegos.addEventListener('click', function() {
+        mostrarContenedorVenta('contenedor-producto-juegos', this);
+    });
 
-btnAccesorios.addEventListener('click', function() {
-    mostrarContenedorVenta('contenedor-producto-accesorios');
-});
+    btnAccesorios.addEventListener('click', function() {
+        mostrarContenedorVenta('contenedor-producto-accesorios', this);
+    });
