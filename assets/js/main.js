6f3b323dbdb9b2b2e288cbf625af0901cc7a4a9a
Ronaldo14715 <ronaldo.farfan.1998@gmail.com>
2024-07-08 22:17:47 -0500

    corrigiendo bug en la pagina ventas

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,20 +1,31 @@
-const abrir = document.querySelector('#abrir');
-    const cerrar = document.querySelector('#cerrar');
-    const nav = document.querySelector('#main-nav');
-
-    // Variables para los botones - VENTAS
+document.addEventListener('DOMContentLoaded', function() {
+        // Variables para los botones - VENTAS
     let btnFigura = document.getElementById('producto-btn-figura');
     let btnFunkoPop = document.getElementById('producto-btn-funkoPop');
     let btnJuegos = document.getElementById('producto-btn-juegos');
     let btnAccesorios = document.getElementById('producto-btn-accesorios');
-
-    // Variables para los DIVS - VENTAS
+    
+        // Variables para los DIVS - VENTAS
     let divFigura = document.getElementById('contenedor-producto-figura');
     let divFunkoPop = document.getElementById('contenedor-producto-funkoPop');
     let divJuegos = document.getElementById('contenedor-producto-juegos');
     let divAccesorios = document.getElementById('contenedor-producto-accesorios');
+    
+    const productos = document.querySelectorAll('.producto');
+    productos.forEach(producto => {
+        producto.style.display = 'none';
+    });
+
+    const figuras = document.getElementById('contenedor-producto-figura');
+    if (figuras) {
+        figuras.style.display = 'grid';
+    }
 
     // NAV
+    const abrir = document.querySelector('#abrir');
+    const cerrar = document.querySelector('#cerrar');
+    const nav = document.querySelector('#main-nav');
+
     abrir.addEventListener('click', () => {
         nav.classList.add("visible");
     });
@@ -57,3 +68,4 @@ const abrir = document.querySelector('#abrir');
     btnAccesorios.addEventListener('click', function() {
         mostrarContenedorVenta('contenedor-producto-accesorios', this);
     });
+});
